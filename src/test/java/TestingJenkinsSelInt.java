2887c4754314b5241cb5be1c8fec34b3e399e99a
Dee <you@example.com>
2017-01-16 13:41:24 -0500

    test codes

---
+++ src/test/java/TestingJenkinsSelInt.java
@@ -0,0 +1,61 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+/**
+ * Created by Kale on 1/14/2017.
+ */
+public class TestingJenkinsSelInt {
+    public String url = "https://www.google.com/";
+    public WebDriver driver;
+    public String propId, propName, propTitle;
+    @BeforeTest
+    public void setup(){
+        System.setProperty("webdriver.chrome.driver", "C:\\SeleniumSetup\\chromedriver_win32\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get(url);
+    }
+
+    @Test
+    public void testWebTitle() {
+        propTitle = "Google";
+        Assert.assertEquals(driver.getTitle(),propTitle,"TestFailed:Expected title not found.");
+        }
+    @Test
+    public void logoCheck(){
+        propId = "hplogo";
+        WebElement actualId = driver.findElement(By.id(propId));
+        if (!(actualId.toString().contains(propId))){
+            Assert.assertFalse(false,"TestFailed:Expected image not found");
+        }
+    }
+    @Test
+    public void searchBarCheck(){
+        propId = "lst-ib";
+        WebElement actualId = driver.findElement(By.id(propId));
+        if (!(actualId.toString().contains(propId))){
+            Assert.assertFalse(false,"TestFailed:Expected search bar not found");
+        }
+    }
+    @Test
+    public void SearchButton(){
+        propName = "btnK";
+        WebElement actualName = driver.findElement(By.name(propName));
+        WebElement actualvalue= driver.findElement(By.cssSelector("input[value='Google Search'][aria-label='Google Search']"));
+        System.out.println("label is: " + actualvalue);
+        if (!(actualName.toString().contains(propName) || actualvalue.toString().contains("Google Search") )){
+            Assert.assertFalse(false,"TestFailed:Expected Search button not found");
+        }
+    }
+
+
+    @AfterTest
+    public void cleanup(){
+        driver.quit();
+    }
+
+
+}
